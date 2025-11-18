20ecda45ea42cf3cc2174b06e390ddc9e25f274a
Aman Negi <negi.aman500@gmail.com>
2025-11-18 11:15:49 +0530

    Added booking logic with transaction

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -5,28 +5,34 @@ const db = require('../models')
 const { ServerConfig } = require('../config');
 const AppError = require('../utils/errors/app-error');
 
-async function createBooking(data) {
-    try {
-        const result = await db.sequelize.transaction(async function bookingImp(t) {
-            try {
-                const flight = await axios.get(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`)
-                const flightData = flight.data.data;
-                if (data.noofSeats > flightData.totalSeats) {
-                    throw new AppError('Not enough seats available', StatusCodes.BAD_REQUEST);
-                }
-                return true;
-            } catch (error) {
-                console.log(error)
-                throw error;
-            }
 
+const bookingRepository = new BookingRepository();
+
+async function createBooking(data) {
+    const transaction = await db.sequelize.transaction();
+    try {
+        const flight = await axios.get(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`);
+        const flightData = flight.data.data;
+        if(data.noofSeats > flightData.totalSeats) {
+            throw new AppError('Not enough seats available', StatusCodes.BAD_REQUEST);
+        }
+        const totalBillingAmount = data.noofSeats * flightData.price;
+        const bookingPayload = {...data, totalCost: totalBillingAmount};
+        const booking = await bookingRepository.create(bookingPayload, transaction);
+
+        await axios.patch(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}/seats`, {
+            seats: data.noofSeats
         });
-        return true
-    } catch (error) {
-        console.log(error)
+
+        await transaction.commit();
+        return booking;
+    } catch(error) {
+        await transaction.rollback();
         throw error;
     }
+    
 }
+
 
 module.exports = {
     createBooking,
